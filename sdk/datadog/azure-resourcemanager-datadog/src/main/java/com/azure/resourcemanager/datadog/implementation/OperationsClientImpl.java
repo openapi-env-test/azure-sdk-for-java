3eaf6336dd4a2fe73a0fd00747392c181f4565ae
SDKAuto <sdkautomation@microsoft.com>
2021-03-23 02:53:00 +0000

    CodeGen from PR 1737 in openapi-env-test/azure-rest-api-specs Merge 5cddd91dda21cc5f4c3ab46cc0f5b48447eb48d7 into d32923f522478e80a7ed196ceb1eaf3ceecfeb4d

--- sdk/datadog/azure-resourcemanager-datadog/src/main/java/com/azure/resourcemanager/datadog/implementation/OperationsClientImpl.java
+++ sdk/datadog/azure-resourcemanager-datadog/src/main/java/com/azure/resourcemanager/datadog/implementation/OperationsClientImpl.java
@@ -81,7 +81,7 @@ public final class OperationsClientImpl implements OperationsClient {
     }
 
     /**
-     * List all operations provided by Microsoft.Datadog for the 2020-02-01-preview api version.
+     * List all operations provided by Microsoft.Datadog for the 2021-03-01 api version.
      *
      * @throws ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
@@ -112,7 +112,7 @@ public final class OperationsClientImpl implements OperationsClient {
     }
 
     /**
-     * List all operations provided by Microsoft.Datadog for the 2020-02-01-preview api version.
+     * List all operations provided by Microsoft.Datadog for the 2021-03-01 api version.
      *
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
@@ -144,7 +144,7 @@ public final class OperationsClientImpl implements OperationsClient {
     }
 
     /**
-     * List all operations provided by Microsoft.Datadog for the 2020-02-01-preview api version.
+     * List all operations provided by Microsoft.Datadog for the 2021-03-01 api version.
      *
      * @throws ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
@@ -156,7 +156,7 @@ public final class OperationsClientImpl implements OperationsClient {
     }
 
     /**
-     * List all operations provided by Microsoft.Datadog for the 2020-02-01-preview api version.
+     * List all operations provided by Microsoft.Datadog for the 2021-03-01 api version.
      *
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
@@ -171,7 +171,7 @@ public final class OperationsClientImpl implements OperationsClient {
     }
 
     /**
-     * List all operations provided by Microsoft.Datadog for the 2020-02-01-preview api version.
+     * List all operations provided by Microsoft.Datadog for the 2021-03-01 api version.
      *
      * @throws ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
@@ -183,7 +183,7 @@ public final class OperationsClientImpl implements OperationsClient {
     }
 
     /**
-     * List all operations provided by Microsoft.Datadog for the 2020-02-01-preview api version.
+     * List all operations provided by Microsoft.Datadog for the 2021-03-01 api version.
      *
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
