92fb1cecf9c76212e1f745413dea62debca04cff
SDKAuto <sdkautomation@microsoft.com>
2022-08-01 07:51:14 +0000

    CodeGen from PR 2400 in test-repo-billy/azure-rest-api-specs Merge bcfed55cc07672a82b11e98feddb0c6e1b2d803c into 161f2cd5033ba059e92a956b87278baf7461823a

--- sdk/appconfiguration/azure-resourcemanager-appconfiguration/src/main/java/com/azure/resourcemanager/appconfiguration/fluent/models/ConfigurationStoreProperties.java
+++ sdk/appconfiguration/azure-resourcemanager-appconfiguration/src/main/java/com/azure/resourcemanager/appconfiguration/fluent/models/ConfigurationStoreProperties.java
@@ -4,18 +4,13 @@
 
 package com.azure.resourcemanager.appconfiguration.fluent.models;
 
-import com.azure.core.annotation.Fluent;
-import com.azure.resourcemanager.appconfiguration.models.CreateMode;
-import com.azure.resourcemanager.appconfiguration.models.EncryptionProperties;
-import com.azure.resourcemanager.appconfiguration.models.PrivateEndpointConnectionReference;
+import com.azure.core.annotation.Immutable;
 import com.azure.resourcemanager.appconfiguration.models.ProvisioningState;
-import com.azure.resourcemanager.appconfiguration.models.PublicNetworkAccess;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.OffsetDateTime;
-import java.util.List;
 
 /** The properties of a configuration store. */
-@Fluent
+@Immutable
 public final class ConfigurationStoreProperties {
     /*
      * The provisioning state of the configuration store.
@@ -34,52 +29,6 @@ public final class ConfigurationStoreProperties {
      */
     @JsonProperty(value = "endpoint", access = JsonProperty.Access.WRITE_ONLY)
     private String endpoint;
-
-    /*
-     * The encryption settings of the configuration store.
-     */
-    @JsonProperty(value = "encryption")
-    private EncryptionProperties encryption;
-
-    /*
-     * The list of private endpoint connections that are set up for this
-     * resource.
-     */
-    @JsonProperty(value = "privateEndpointConnections", access = JsonProperty.Access.WRITE_ONLY)
-    private List<PrivateEndpointConnectionReference> privateEndpointConnections;
-
-    /*
-     * Control permission for data plane traffic coming from public networks
-     * while private endpoint is enabled.
-     */
-    @JsonProperty(value = "publicNetworkAccess")
-    private PublicNetworkAccess publicNetworkAccess;
-
-    /*
-     * Disables all authentication methods other than AAD authentication.
-     */
-    @JsonProperty(value = "disableLocalAuth")
-    private Boolean disableLocalAuth;
-
-    /*
-     * The amount of time in days that the configuration store will be retained
-     * when it is soft deleted.
-     */
-    @JsonProperty(value = "softDeleteRetentionInDays")
-    private Integer softDeleteRetentionInDays;
-
-    /*
-     * Property specifying whether protection against purge is enabled for this
-     * configuration store.
-     */
-    @JsonProperty(value = "enablePurgeProtection")
-    private Boolean enablePurgeProtection;
-
-    /*
-     * Indicates whether the configuration store need to be recovered.
-     */
-    @JsonProperty(value = "createMode")
-    private CreateMode createMode;
 
     /**
      * Get the provisioningState property: The provisioning state of the configuration store.
@@ -109,152 +58,10 @@ public final class ConfigurationStoreProperties {
     }
 
     /**
-     * Get the encryption property: The encryption settings of the configuration store.
-     *
-     * @return the encryption value.
-     */
-    public EncryptionProperties encryption() {
-        return this.encryption;
-    }
-
-    /**
-     * Set the encryption property: The encryption settings of the configuration store.
-     *
-     * @param encryption the encryption value to set.
-     * @return the ConfigurationStoreProperties object itself.
-     */
-    public ConfigurationStoreProperties withEncryption(EncryptionProperties encryption) {
-        this.encryption = encryption;
-        return this;
-    }
-
-    /**
-     * Get the privateEndpointConnections property: The list of private endpoint connections that are set up for this
-     * resource.
-     *
-     * @return the privateEndpointConnections value.
-     */
-    public List<PrivateEndpointConnectionReference> privateEndpointConnections() {
-        return this.privateEndpointConnections;
-    }
-
-    /**
-     * Get the publicNetworkAccess property: Control permission for data plane traffic coming from public networks while
-     * private endpoint is enabled.
-     *
-     * @return the publicNetworkAccess value.
-     */
-    public PublicNetworkAccess publicNetworkAccess() {
-        return this.publicNetworkAccess;
-    }
-
-    /**
-     * Set the publicNetworkAccess property: Control permission for data plane traffic coming from public networks while
-     * private endpoint is enabled.
-     *
-     * @param publicNetworkAccess the publicNetworkAccess value to set.
-     * @return the ConfigurationStoreProperties object itself.
-     */
-    public ConfigurationStoreProperties withPublicNetworkAccess(PublicNetworkAccess publicNetworkAccess) {
-        this.publicNetworkAccess = publicNetworkAccess;
-        return this;
-    }
-
-    /**
-     * Get the disableLocalAuth property: Disables all authentication methods other than AAD authentication.
-     *
-     * @return the disableLocalAuth value.
-     */
-    public Boolean disableLocalAuth() {
-        return this.disableLocalAuth;
-    }
-
-    /**
-     * Set the disableLocalAuth property: Disables all authentication methods other than AAD authentication.
-     *
-     * @param disableLocalAuth the disableLocalAuth value to set.
-     * @return the ConfigurationStoreProperties object itself.
-     */
-    public ConfigurationStoreProperties withDisableLocalAuth(Boolean disableLocalAuth) {
-        this.disableLocalAuth = disableLocalAuth;
-        return this;
-    }
-
-    /**
-     * Get the softDeleteRetentionInDays property: The amount of time in days that the configuration store will be
-     * retained when it is soft deleted.
-     *
-     * @return the softDeleteRetentionInDays value.
-     */
-    public Integer softDeleteRetentionInDays() {
-        return this.softDeleteRetentionInDays;
-    }
-
-    /**
-     * Set the softDeleteRetentionInDays property: The amount of time in days that the configuration store will be
-     * retained when it is soft deleted.
-     *
-     * @param softDeleteRetentionInDays the softDeleteRetentionInDays value to set.
-     * @return the ConfigurationStoreProperties object itself.
-     */
-    public ConfigurationStoreProperties withSoftDeleteRetentionInDays(Integer softDeleteRetentionInDays) {
-        this.softDeleteRetentionInDays = softDeleteRetentionInDays;
-        return this;
-    }
-
-    /**
-     * Get the enablePurgeProtection property: Property specifying whether protection against purge is enabled for this
-     * configuration store.
-     *
-     * @return the enablePurgeProtection value.
-     */
-    public Boolean enablePurgeProtection() {
-        return this.enablePurgeProtection;
-    }
-
-    /**
-     * Set the enablePurgeProtection property: Property specifying whether protection against purge is enabled for this
-     * configuration store.
-     *
-     * @param enablePurgeProtection the enablePurgeProtection value to set.
-     * @return the ConfigurationStoreProperties object itself.
-     */
-    public ConfigurationStoreProperties withEnablePurgeProtection(Boolean enablePurgeProtection) {
-        this.enablePurgeProtection = enablePurgeProtection;
-        return this;
-    }
-
-    /**
-     * Get the createMode property: Indicates whether the configuration store need to be recovered.
-     *
-     * @return the createMode value.
-     */
-    public CreateMode createMode() {
-        return this.createMode;
-    }
-
-    /**
-     * Set the createMode property: Indicates whether the configuration store need to be recovered.
-     *
-     * @param createMode the createMode value to set.
-     * @return the ConfigurationStoreProperties object itself.
-     */
-    public ConfigurationStoreProperties withCreateMode(CreateMode createMode) {
-        this.createMode = createMode;
-        return this;
-    }
-
-    /**
      * Validates the instance.
      *
      * @throws IllegalArgumentException thrown if the instance is not valid.
      */
     public void validate() {
-        if (encryption() != null) {
-            encryption().validate();
-        }
-        if (privateEndpointConnections() != null) {
-            privateEndpointConnections().forEach(e -> e.validate());
-        }
     }
 }
