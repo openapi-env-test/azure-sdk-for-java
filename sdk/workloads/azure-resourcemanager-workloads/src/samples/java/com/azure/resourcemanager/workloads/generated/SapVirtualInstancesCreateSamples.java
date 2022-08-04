2aaae38a3e75bf3d213995088763e77f452238e3
SDKAuto <sdkautomation@microsoft.com>
2022-08-04 03:26:43 +0000

    CodeGen from PR 2551 in test-repo-billy/azure-rest-api-specs Merge 2a20c2b943f99b46aaf11e73156898fddbf887d2 into 19a4a9a0dbc93f22971facac03ab783ce892dde1

--- sdk/workloads/azure-resourcemanager-workloads/src/samples/java/com/azure/resourcemanager/workloads/generated/SapVirtualInstancesCreateSamples.java
+++ sdk/workloads/azure-resourcemanager-workloads/src/samples/java/com/azure/resourcemanager/workloads/generated/SapVirtualInstancesCreateSamples.java
@@ -10,6 +10,7 @@ import com.azure.resourcemanager.workloads.models.DatabaseConfiguration;
 import com.azure.resourcemanager.workloads.models.DeployerVmPackages;
 import com.azure.resourcemanager.workloads.models.DeploymentConfiguration;
 import com.azure.resourcemanager.workloads.models.DeploymentWithOSConfiguration;
+import com.azure.resourcemanager.workloads.models.DiscoveryConfiguration;
 import com.azure.resourcemanager.workloads.models.HighAvailabilityConfiguration;
 import com.azure.resourcemanager.workloads.models.ImageReference;
 import com.azure.resourcemanager.workloads.models.LinuxConfiguration;
@@ -589,6 +590,31 @@ public final class SapVirtualInstancesCreateSamples {
                                                                         new SshPublicKey()
                                                                             .withKeyData("ssh-rsa public key")))))))))
             .withTags(mapOf())
+            .create();
+    }
+
+    /*
+     * x-ms-original-file: specification/workloads/resource-manager/Microsoft.Workloads/preview/2021-12-01-preview/examples/sapvirtualinstances/SAPVirtualInstances_Create_Discover.json
+     */
+    /**
+     * Sample code: Register existing SAP system as Virtual Instance for SAP solutions.
+     *
+     * @param manager Entry point to WorkloadsManager.
+     */
+    public static void registerExistingSAPSystemAsVirtualInstanceForSAPSolutions(
+        com.azure.resourcemanager.workloads.WorkloadsManager manager) {
+        manager
+            .sapVirtualInstances()
+            .define("X00")
+            .withRegion("northeurope")
+            .withExistingResourceGroup("test-rg")
+            .withEnvironment(SapEnvironmentType.NON_PROD)
+            .withSapProduct(SapProductType.S4HANA)
+            .withConfiguration(
+                new DiscoveryConfiguration()
+                    .withCentralServerVmId(
+                        "/subscriptions/8e17e36c-42e9-4cd5-a078-7b44883414e0/resourceGroups/test-rg/providers/Microsoft.Compute/virtualMachines/sapq20scsvm0"))
+            .withTags(mapOf("createdby", "abc@microsoft.com", "test", "abc"))
             .create();
     }
 
